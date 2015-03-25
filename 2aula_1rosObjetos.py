80506b9132dbcc6932c9ffbca407084c681e75b0
Adolfo Correa <fitomillennium@hotmail.com>
2015-03-25 15:28:03 -0300

    Exercicio completo
    Se completou o exercicio 3 e arrumou os prints com os parentesis para
    poder ser rodado no python 3 e no 2.
    Tbm se adionou um docstring para a classe da bomba de combustivel
    (exercicio 3)

--- 2aula_1rosObjetos.py
+++ 2aula_1rosObjetos.py
@@ -5,9 +5,9 @@
 ##        return self.volume
 
 
-print "\n\n\n***************************"
-print "\t1o Exercicio"
-print "***************************\n"
+print ("\n\n\n***************************")
+print ("\t1o Exercicio")
+print ("***************************\n")
 
 class quadrado:
     def __init__(self,lado=1):
@@ -21,18 +21,30 @@ class quadrado:
 
     def area(self):
         return self.lado**2
+        
+#Print Python 2
 
+#print ("\nQuadrado\n")
+#quad1=quadrado(5)
+#print "quadrado(5)=",quad1.get_lado()
+#quad1.mudar_lado(20)
+#print "quad1.mudar_lado(20)",quad1.get_lado()
+#quad1.area()
+#print "quad1.area()",quad1.area()
 
+#Print Python 3
+
+print ("\nQuadrado\n")
 quad1=quadrado(5)
-print "quadrado(5)=",quad1.get_lado()
+print ("quadrado(5)=",quad1.get_lado())
 quad1.mudar_lado(20)
-print "quad1.mudar_lado(20)",quad1.get_lado()
+print ("quad1.mudar_lado(20)",quad1.get_lado())
 quad1.area()
-print "quad1.area()",quad1.area()
+print ("quad1.area()",quad1.area())
 
-print "\n\n\n***************************"
-print "\t2o Exercicio"
-print "***************************\n"
+print ("\n\n\n***************************")
+print ("\t2o Exercicio")
+print ("***************************\n")
 
 
 class ponto:
@@ -79,45 +91,208 @@ class Triangulo:
         area=(p*(p-ab)*(p-bc)*(p-ca))**0.5
         return area
 
-print "\nItem a)"
+print ("\nTriangulo\n")
+        
+print ("\nItem a)")
 A=ponto(0,0,0)
 B=ponto(1,1,0)
 C=ponto(1,1,1)
-print "A=punto(0,0,0)=", A.x,A.y,A.z
-print "B=ponto(1,1,1)=", B.x,B.y,B.z
-print "C=ponto(1,1,0)=", C.x,C.y,C.z
 
-print "\nItem b)"
+#Print Python 2
+
+#print "A=punto(0,0,0)=", A.x,A.y,A.z
+#print "B=ponto(1,1,1)=", B.x,B.y,B.z
+#print "C=ponto(1,1,0)=", C.x,C.y,C.z
+#
+#print ("\nItem b)")
+#AB=linha(A,B)
+#print "AB.comprimento() = ", AB.comprimento()
+#
+#print ("\nItem c)")
+#ABC=Triangulo(A,B,C)
+#print "ABC.lados() = ", ABC.lados()
+#print "ABC.area() = ", ABC.area()
+
+#Print Python 3
+print ("A=punto(0,0,0)=", A.x,A.y,A.z)
+print ("B=ponto(1,1,1)=", B.x,B.y,B.z)
+print ("C=ponto(1,1,0)=", C.x,C.y,C.z)
+
+print ("\nItem b)")
 AB=linha(A,B)
-print "AB.comprimento() = ", AB.comprimento()
+print ("AB.comprimento() = ", AB.comprimento())
 
-print "\nItem c)"
+print ("\nItem c)")
 ABC=Triangulo(A,B,C)
-print "ABC.lados() = ", ABC.lados()
-print "ABC.area() = ", ABC.area()
+print ("ABC.lados() = ", ABC.lados())
+print ("ABC.area() = ", ABC.area())
 
 
-print "\n\n\n***************************"
-print "\t3o Exercicio"
-print "***************************\n"
+print ("\n\n\n***************************")
+print ("\t3o Exercicio")
+print ("***************************\n")
+
+# class bombaCombustivel:
+#     def __init__(self,tipoCombustivel,valorLitro,quantidadeCombustivel=0):
+#         self.tipoCombustivel=tipoCombustivel
+#         self.valorLitro=valorLitro
+#         self.quantidadeCombustivel=quantidadeCombustivel
+#     def abastecerPorValor(self,recarga):
+#         self.quantidadeCombustivel+=float(recarga)/self.valorLitro
+#         return self.quantidadeCombustivel
+#     def abastecerPorLitro(self,recarga):
+#         self.quantidadeCombustivel+=recarga
+#         return self.quantidadeCombustivel
+#     def alterarValor(self,nwValor):
+#         self.valorLitro=nwValor
+#     def alterarCombustivel(self,nwtipo):
+#         self.tipoCombustivel=nwtipo
+#     def alterarQCombutivel(self,nwQuantidade):
+#         self.quantidadeCombustivel=nwQuantidade
+
 
 class bombaCombustivel:
+    """bombaCombustivel(T,x,y)
+
+Trigonometric sine, element-wise.
+
+Parameters
+----------
+T : String
+    Tipo do Combustivel
+x : float
+    Valor do combustivel por litro.
+y : float
+    Quantidade no tanque.
+    
+
+Atributos
+-------
+T : String
+    Tipo do Combustivel
+x : float
+    Valor do combustivel por litro.
+y : float
+    Quantidade no tanque.
+
+Methods
+--------
+
+abastecerPorValor(x):
+    x:  float
+        Recarga em valor monetario.
+abastecerPorLitro(x):
+    x:  float
+        Recarga em litros.
+alterarValor(x):
+    x:  float
+        Novo Valor do combustivel por litro.
+alterarCombustivel(T):
+    T : String
+         Novo Tipo do Combustivel
+alterarQCombutivel(y):
+    y : float
+        Actualizacao da Quantidade no tanque.
+
+Notes
+-----
+Exercicios da 2a Semana de Aula de Computacao UFRJ com o 
+Professor Igor Leao.
+
+Examples
+--------
+Inicializacao:
+
+>>> bomba1=bombaCombustivel("Diesel",50.40,1000)
+
+
+Print abastecimento por valor:
+
+>>> bomba1.abastecerPorValor(150)
+
+Print abastecimento por litro:
+
+>>> bomba1.abastecerPorLitro(20)
+"""
     def __init__(self,tipoCombustivel,valorLitro,quantidadeCombustivel=0):
         self.tipoCombustivel=tipoCombustivel
         self.valorLitro=valorLitro
         self.quantidadeCombustivel=quantidadeCombustivel
+    
     def abastecerPorValor(self,recarga):
-        #corregir
-        self.quantidadeCombustivel+=float(recarga)/self.valorLitro
-        return self.quantidadeCombustivel
+        Lts=float(recarga)/self.valorLitro
+        self.alterarQCombutivel(Lts)
+        return Lts
+        
     def abastecerPorLitro(self,recarga):
-        self.quantidadeCombustivel+=recarga
-        return self.quantidadeCombustivel
+        self.alterarQCombutivel(recarga)
+        pagar=recarga*self.valorLitro
+        return pagar
+        
     def alterarValor(self,nwValor):
         self.valorLitro=nwValor
+        
     def alterarCombustivel(self,nwtipo):
         self.tipoCombustivel=nwtipo
-    def alterarQCombutivel(self,nwQuantidade):
-        self.quantidadeCombustivel=nwQuantidade
         
-    
+    def alterarQCombutivel(self,saida):
+        self.quantidadeCombustivel-=saida
+
+
+   
+print ("\nBomba de Combustivel\n")
+
+#Print Python 2
+
+##Inicializacao
+#bomba1=bombaCombustivel("Diesel",50.40,1000)
+#print "\nBombaCombustivel(\"Diesel\",50.40,1000)=",\
+#bomba1.tipoCombustivel,bomba1.valorLitro,bomba1.quantidadeCombustivel
+#
+##AbastecerPorLitro
+#print "\nAbastecerPorLitro(20)= R$ ",bomba1.abastecerPorLitro(20)
+#print "Bomba1= ", bomba1.tipoCombustivel,bomba1.valorLitro,\
+#bomba1.quantidadeCombustivel
+#
+##AbastecerPorValor
+#print "\nAbastecerPorValor(150)= Lts ",bomba1.abastecerPorValor(150)
+#print "Bomba1= ", bomba1.tipoCombustivel,\
+#bomba1.valorLitro,bomba1.quantidadeCombustivel
+#
+##AlterarValor
+#print "\nAlterarValor(30.32)= R$ ",bomba1.alterarValor(30.32)
+#print "Bomba1= ", bomba1.tipoCombustivel,bomba1.valorLitro,\
+#bomba1.quantidadeCombustivel
+#
+##Alterar Tipo do Combustivel
+#print "\nAlterarCombustivel(\"Gasolina\")",bomba1.alterarCombustivel("Gasolina")
+#print "Bomba1= ", bomba1.tipoCombustivel,bomba1.valorLitro,\
+#bomba1.quantidadeCombustivel
+
+#Print Python 3
+
+#Inicializacao
+bomba1=bombaCombustivel("Diesel",50.40,1000)
+print ("\nBombaCombustivel(\"Diesel\",50.40,1000)=",\
+bomba1.tipoCombustivel,bomba1.valorLitro,bomba1.quantidadeCombustivel)
+
+#AbastecerPorLitro
+print ("\nAbastecerPorLitro(20)= R$ ",bomba1.abastecerPorLitro(20))
+print ("Bomba1= ", bomba1.tipoCombustivel,bomba1.valorLitro,\
+bomba1.quantidadeCombustivel)
+
+#AbastecerPorValor
+print ("\nAbastecerPorValor(150)= Lts ",bomba1.abastecerPorValor(150))
+print ("Bomba1= ", bomba1.tipoCombustivel,\
+bomba1.valorLitro,bomba1.quantidadeCombustivel)
+
+#AlterarValor
+print ("\nAlterarValor(30.32)= R$ ",bomba1.alterarValor(30.32))
+print ("Bomba1= ", bomba1.tipoCombustivel,bomba1.valorLitro,\
+bomba1.quantidadeCombustivel)
+
+#Alterar Tipo do Combustivel
+print ("\nAlterarCombustivel(\"Gasolina\")",bomba1.alterarCombustivel("Gasolina"))
+print ("Bomba1= ", bomba1.tipoCombustivel,bomba1.valorLitro,\
+bomba1.quantidadeCombustivel)
+
