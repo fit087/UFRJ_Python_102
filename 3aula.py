a33d1995d45c67192da70683ea12d7087de774a0
Adolfo Correa <fitomillennium@hotmail.com>
2015-04-08 09:57:57 -0300

    Property
    A mascara property nao modificou o atributo

--- 3aula.py
+++ 3aula.py
@@ -15,11 +15,24 @@ class Carro:
         return cmp(self.__nrodas, car.get_nr())
     def get_nr(self):
         return self.__nrodas
+    def set_nr(self,nr):
+        self.__nrodas = nr
+        
+    rodas=property(get_nr,set_nr,doc="Numero de rodas do carro")
+    #property()
 
 a=Carro(4);b=Carro(6)
 print a + b
 print a>b
+print cmp(4,6)
 print a
+
+print a.rodas
+a.rodas=2
+
+print a.rodas
+print a.get_nr()
+#help(a.rodas())
 
 #a + b
 #a>b
@@ -29,3 +42,5 @@ class objeto(object):
     pass
 
 obj=objeto()
+
+
