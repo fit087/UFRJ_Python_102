cf96a3e5d05cae3690a8f029fe2a028dd5eb1e36
Adolfo Correa <fitomillennium@hotmail.com>
2015-04-16 15:46:11 -0300

    Files Exc 1
    Aula Teorica de Arquivos
    Excercicio 1 da lista de arquivos

---
+++ 5files.py
@@ -0,0 +1,47 @@
+# -*- coding: latin1 -*-
+# 16/06/2015
+"""
+Created on Thu Apr 16 14:20:32 2015
+    Lista de Exercicios:        Arquivos
+    File:                       lista Arquivos.pdf
+    Curso:                      Computacao II (MAB225)
+    Proffessor:                 Igor Leao
+@author: Adolfo Emmanuel Correa Lopez
+"""
+
+import random
+
+# Exercise 1
+
+def random_txt():
+    "Make a file with random lines"
+    resultado = ""
+    for linha in range(300):
+            for i in range(10):
+                    resultado += chr(random.randint(ord("a"),ord("z")))
+            resultado += "\n"
+
+    open("5file.txt", "w").write(resultado)
+
+def conta():
+    "Count the frequency of any letter in the file"
+    #alfabeto = [0]*26
+    alfabeto = [0 for i in range(26)]
+    conteudo = open("5file.txt", "r").readlines()
+    for l in conteudo:
+        for c in l[:-2]: #l[:-2] # tem um \n (newline) q foi lido p/ readlines
+            alfabeto[ord(c) - ord("a")] += 1
+    return alfabeto
+
+def print_dic(lista_dic):
+    "Print in format of dictionary"
+    for i, j in enumerate(lista_dic):
+        print chr(i + ord("a")) + ":" + str(j)
+
+if __name__ == '__main__':
+
+    # Exercise 1
+    
+    random_txt()
+    valores = conta()
+    print_dic(valores)
